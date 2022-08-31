255814ff7df4ad6a8500ef42db8569ff842ad16b
sayronbr1010 <98848566+sayronbr1010@users.noreply.github.com>
2022-08-31 19:57:49 -0300

    ex18

---
+++ 18.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Programa
+{
+	public static void Main(string[] args)
+	{
+        double n1,n2,n3;
+
+        Console.WriteLine("digite todos os lados de um triangulo");
+        n1=double.Parse( Console.ReadLine());
+        n2=double.Parse( Console.ReadLine());
+        n3=double.Parse( Console.ReadLine());
+
+        if(n1==n2 && n2==n3){
+            Console.WriteLine("ele é EQUILÁTERO");
+        }if(n1==n2 && n2!=n3||(n2==n3 && n3!=n1)||(n3==n1&&n1!=n2)){
+            Console.WriteLine("ele é ISÓSCELES");
+        } if(n1!=n2 && n2!=n3 && n3!=n1){
+            Console.WriteLine("ele é ESCALENO");
+        }
+	}
+}
